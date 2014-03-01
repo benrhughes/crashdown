dab501c2882f2fd96dfb6c4128cb37070edb6478
Ben <ben@benrhughes.com>
2014-03-01 23:51:37 +1100

    Refactoring to be more callbacky

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var page = require('./lib/page.js');
 
 var cons = require('consolidate');
 var path = require('path');
+var _ = require('underscore');
+
 var express = require('express');
 var app = express();
 
@@ -14,21 +16,30 @@ app.engine('html', cons.hogan);
 app.set('view engine', 'html');
 app.set('views', path.join(__dirname, themesDir, 'templates'));
 
+page.setOptions({pagesDir: path.join(__dirname, 'pages')});
+
 // routes
 app.get('/favicon.ico', function(req, res){
 	res.sendfile(path.join(__dirname, 'favicon.ico'));
 });
 
+function render(res, err, template, model){
+	if(err)
+		return res.send(err, template);
+
+	res.render(template, model);
+}
+
 app.get('/', function(req, res){
-	page.pages(res, __dirname, 0);
+	page.pages(0, _.partial(render, res));
 });
 
 app.get('/all', function(req, res){
-	page.listAll(res, __dirname);
+	page.listAll(_.partial(render, res));
 });
 
 app.get('/pages/:num', function(req, res){
-	page.pages(res, __dirname, req.params.num);
+	page.pages(req.params.num, _.partial(render, res));
 });
 
 // get /search?q=term
@@ -37,15 +48,21 @@ app.get('/search*', function(req, res){
 });
 
 app.get('/rss', function(req, res){
-	page.rss(res,__dirname);
+	page.rss(function(err, template, model){
+		if(err)
+			return res.send(err, template);
+
+		res.type('application/xml');
+		res.render(template, model);
+	});
 });
 
 app.get('/tags', function(req, res){
-	page.tags(res);
+	page.tags(_.partial(render, res));
 });
 
 app.get('/tag/:tag', function(req,res){
-	page.tag(res, req.params.tag);
+	page.tag(req.params.tag, _.partial(render, res));
 });
 
 app.get('/uploads/*', function(req, res){
@@ -61,7 +78,7 @@ app.get('/static/*', function(req, res){
 });
 
 app.get('/:slug', function(req, res){
-	page.page(res, __dirname, '',  req.params.slug);
+	page.page('', req.params.slug, _.partial(render, res));
 });
 
 // GET /some/long/path/to/file
@@ -69,11 +86,11 @@ app.get('/*/:path', function(req, res){
 	var path = req.params.path;
 	var slug = req.params[0];
 
-	page.page(res, __dirname, path, slug);
+	page.page(path, slug, _.partial(render, res));
 });
 
-page.populateCache(__dirname);
-page.watchCache(__dirname);
+page.populateCache();
+page.watchCache();
 
 var port = config.port || 80; 
 app.listen(port);
